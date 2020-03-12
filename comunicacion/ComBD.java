9cc095f7411bedd082adac289ca96c418b0acabf
Urbil27 <urbiltxo@gmail.com>
2020-03-12 11:36:51 +0100

    creo clase ComBD

---
+++ comunicacion/ComBD.java
@@ -0,0 +1,21 @@
+package comunicacion;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class ComBD {
+	private String nombre ="bdt-concesionario";
+	private String url ="jdbc:mysql://localhost";
+	private String usuario = "root";
+	private String contraseña = "root";
+	public Connection Conectar() {
+		Connection link = null;
+		try {
+			Class.forName("org.gjt.mm.mysql.Driver");
+			link = DriverManager.getConnection(this.url,this.usuario,this.contraseña);
+		}
+		catch(Exception ex) {
+			JOptionPane.showMessageDialog(null, ex);
+		}
+		return link;
+	}
+}
