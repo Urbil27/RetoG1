8ff9488ae75c328afd84d6bc3d82c80c49cbaeeb
Urbil27 <urbiltxo@gmail.com>
2020-03-13 13:29:38 +0100

    clase ComBD creado el constructor que establece la conexion

--- comunicacion/ComBD.java
+++ comunicacion/ComBD.java
@@ -4,24 +4,27 @@ import java.sql.*;
 import javax.swing.JOptionPane;
 
 
+
 public class ComBD {
-	public String color;
-	public vehiculos.Vehiculo v;
-	private String nombre ="bdt-concesionario";
-	private String url ="jdbc:mysql://localhost";
-	private String usuario = "root";
-	private String contraseña = "root";
-	public Connection Conectar() {
-		Connection link = null;
+	
+	ComBD(){
 		try {
-			Class.forName("org.gjt.mm.mysql.Driver");
-			link = DriverManager.getConnection(this.url,this.usuario,this.contraseña);
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			DriverManager.getConnection("jdbc://mysql://localhost:3306/btd_concesionario","root", "root");
+			System.out.println("Conexión OK");
+		} catch(ClassNotFoundException e){
+			System.out.println("Error al cargar el controlador");
+		} catch (SQLException e) {
+			System.out.println("Error en la conexion");
+			e.printStackTrace();
 		}
-		catch(Exception ex) {
-			JOptionPane.showMessageDialog(null, ex);
-		}
-		return link;
+		
+		
 	}
+	
+	
+	
+	
 	void consultaCoche() {
 		
 	}
