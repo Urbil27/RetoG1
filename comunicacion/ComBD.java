775801074cb9cbf3f65e14e4b501c9f17b9b3a32
Urbil27 <urbiltxo@gmail.com>
2020-03-16 10:04:20 +0100

    Mejora de la clase ComBD

--- comunicacion/ComBD.java
+++ comunicacion/ComBD.java
@@ -1,27 +1,44 @@
 package comunicacion;
 
-import java.sql.*;
-import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 
 public class ComBD {
+	private static final String CONTROLADOR ="com.mysql.cj.jdbc.Driver";
+	private static final String URL="jdbc:mysql://localhost:3306/BDT_CONCESIONARIO";
+	private static final String USUARIO ="root";
+	private static final String CONTRASEÑA="root";
 	
-	ComBD(){
+	static {
+		 try {
+			 Class.forName(CONTROLADOR);
+		 } 
+		 catch(ClassNotFoundException e) {
+			 System.out.println("Error al cargar el controlador");
+			 e.printStackTrace();
+		 }
+	}
+	
+	
+	
+	public Connection Conectar() {
+		Connection conexion = null;
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			DriverManager.getConnection("jdbc:mysql://localhost:3306/BDT_CONCESIONARIO","root", "root");
+			
+			conexion = DriverManager.getConnection(URL,USUARIO, CONTRASEÑA);
 			System.out.println("Conexión OK");
-		} catch(ClassNotFoundException e){
-			System.out.println("Error al cargar el controlador");
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e) {
 			System.out.println("Error en la conexion");
 			e.printStackTrace();
 		}
 		
-		
+		return conexion;
 	}
-	
+
 	
 	
 	
--- comunicacion/prueba.java
+++ comunicacion/prueba.java
@@ -4,7 +4,8 @@ public class prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- ComBD c = new ComBD();
+ ComBD conexion = new ComBD();
+ conexion.Conectar();
 	}
 
 }
